9e95e3dcdace48de89ce2e6c73707d59cc49a288
KerryChen <kerry.chen@thinkpower-info.com>
2019-11-07 14:09:56 +0800

    modified gitignore

---
+++ build/precache-manifest.2abcfc6c5019e4ca7f49ebce3d105035.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37fd99a9275d25c0e023401e928bec6b",
+    "url": "/React-youtube-API/build/index.html"
+  },
+  {
+    "revision": "6d75c5e6f525323b0e72",
+    "url": "/React-youtube-API/build/static/css/main.89ac8fc5.chunk.css"
+  },
+  {
+    "revision": "2d0751566b0f234c2e94",
+    "url": "/React-youtube-API/build/static/js/2.f1b9daee.chunk.js"
+  },
+  {
+    "revision": "6d75c5e6f525323b0e72",
+    "url": "/React-youtube-API/build/static/js/main.900c663d.chunk.js"
+  },
+  {
+    "revision": "0baa641359e0ffe63bdc",
+    "url": "/React-youtube-API/build/static/js/runtime-main.3cdd9dad.js"
+  }
+]);
